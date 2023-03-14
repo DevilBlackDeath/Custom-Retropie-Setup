a40279022018054f03b52f34664539c5dfe1f32a
DBD <blackcheytan@gmail.com>
2023-03-14 13:31:51 +0100

    Create presetup.sh

---
+++ presetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt -y install libsamplerate0-dev
+sudo apt -y install git lsb-release
+cd
+git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+cd RetroPie-Setup
+chmod +x retropie_setup.sh
+sudo ./retropie_setup.sh
